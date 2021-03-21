0ab0bed40ba71158d9470398cf43539ed9350b4c
MrMohebi <MMMohebi@outlook.com>
2021-03-21 17:41:31 +0330

    create tokens.php v2, change test token

---
+++ v2/token/tokens.php
@@ -0,0 +1,8 @@
+<?php
+
+
+class tokens
+{
+    const PAYPING_TEST = "9fb9bebbdb1d6017b3a130e584d850ae965db92440a5c42fb3ad4895e9e726ee";
+    const PAYPING_CUKI = "545256965a62cf4de51980b366aa1576e5d37b1d156761fefae177ed3ba35905";
+}
